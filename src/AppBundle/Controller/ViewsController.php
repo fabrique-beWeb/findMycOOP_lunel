ec3d8575a4f6f3ef6327f071bad8fdf2c7d3a31f
mathieu becerra <matheiu.becerra.beweb@gmail.com>
2017-01-12 16:55:59 +0100

    Ajout fonction dans ViewsController.

--- src/AppBundle/Controller/ViewsController.php
+++ src/AppBundle/Controller/ViewsController.php
@@ -21,6 +21,13 @@ class ViewsController extends Controller {
     public function getProjetsPage() {
         return $this->render(':site:projets.html.twig');
     }
+    /**
+     * @Route("/details", name="details")
+     */
+
+    public function getDetailsPage() {
+        return $this->render(':site:details.html.twig');
+    }
 
     /**
      * @Route("/membres", name="membres")
@@ -36,6 +43,27 @@ class ViewsController extends Controller {
 
     public function getBar() {
          return $this->render(':site:bars.html.twig');
+    }
+    /**
+     * @Route("/contact", name="contact")
+     */
+
+    public function getContactPage() {
+         return $this->render(':site:contact.html.twig');
+    }
+    /**
+     * @Route("/carnet", name="carnetdebord")
+     */
+
+    public function getCarnetDeBordPage() {
+         return $this->render(':site:carnetdebord.html.twig');
+    }
+    /**
+     * @Route("/login", name="login")
+     */
+
+    public function getLogin() {
+         return $this->render(':site:login.html.twig');
     }
 
 }
