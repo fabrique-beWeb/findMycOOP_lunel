6241bb0839611f947bd19b378ecac689196f024d
sebeweb <sebbeweb@gmail.fr>
2017-02-02 08:42:30 +0100

    ajout css pour les boutons dans carnet de bord

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
         var subject = $(".sujet").val();
         var message = $(".message").val();
         $.ajax({
-            url: "http://localhost/fmcTest/web/app_dev.php/envoieMail",
+            url: "http://localhost/FindMyTest/web/app_dev.php/envoieMail",
             data: {"mail": mail, "sujet": subject, "message": message},
             type: 'get',
             dataType: 'json',
@@ -35,23 +35,39 @@ $(document).ready(function () {
 
 var positionBase;
 var pageShow;
+var btnSelect;
+var url;
+var carnet = "http://localhost/FindMyTest/web/app_dev.php/carnet";
 $(window).load(function () {
     positionBase = $(".evenements").position().top;
 //    $("#projet").hide();
+//    var pathname = window.location.pathname;
+    url = window.location.href;
+    if (url != carnet) {
+        btnSelect.css({marginLeft: 20 + "px", color: "white", backgroundColor: "darkslategrey"});
+    }
+
+    btnSelect = $(".trio > li[id = projet]").css({marginLeft: 0 + "px", color: "darkslategrey", backgroundColor: "white"});
     $(".carnet > section").hide();
-    pageShow = $(".carnet > section[id = projet]").show().css({marginLeft: 0 + "px"});
+    pageShow = $(".carnet > section[id = projet]").show();
 });
 
 $(window).scroll(function () {
     var newPosition = positionBase - $("body").scrollTop();
-    if ($("body").scrollTop() < $("nav").height()) {
+    if ($("body").scrollTop() < $("nav").height() + $("header").height()) {
         $(".evenements").css({top: newPosition + "px"});
     }
 });
 
 $(".trio > li").click(function () {
+    if (url != carnet) {
+        window.location = "http://localhost/FindMyTest/web/app_dev.php/carnet";
+    }
     var id = $(this).attr("id");
     pageShow.hide();
+    btnSelect.css({marginLeft: 20 + "px", color: "white", backgroundColor: "darkslategrey"});
+    $(".trio > li[id =" + id + "").css({marginLeft: 0 + "px", color: "darkslategrey", backgroundColor: "white"});
     $(".carnet > section[id=" + id + "]").show();
     pageShow = $(".carnet > section[id=" + id + "]");
+    btnSelect = $(".trio > li[id =" + id + "");
 });
