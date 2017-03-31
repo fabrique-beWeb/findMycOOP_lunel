3b6413bb50217568b070b4f74949226c7aa5593a
fflachet <fflachet@ff-beweb-lunel>
2017-03-31 09:57:04 +0200

    correction affichage projets forum

--- web/scripts/projects.js
+++ web/scripts/projects.js
@@ -4,16 +4,11 @@ monAppli.controller('projectCtrl', [
     '$scope', '$http', function ($scope, $http) {
 
 
-        $scope.showSousThemes = true;
-        $scope.showProjects = false;
-        $scope.showTasks = false;
-        $scope.showPosts = false;
 
         $scope.themes = {};
         $http.get("themes")
                 .then(function (response) {
                     $scope.themes = response.data;
-                    $scope.showSousThemes = true;
 
                 });
 
@@ -22,43 +17,37 @@ monAppli.controller('projectCtrl', [
         $scope.getSousThemesFromTheme = function (id) {
             $http.get("sousTheme/" + id)
                     .then(function (response) {
-                        $scope.showSousThemes = true;
                         $scope.sousThemes = response.data;
                     });
         };
         $scope.getSousThemes = function () {
             $http.get("sousTheme")
                     .then(function (response) {
-                        $scope.showSousThemes = true;
                         $scope.sousThemes = response.data;
                     });
         };
 
         $scope.projects = {};
+        $scope.projectsEvents = {};
 //        $scope.getProjects = function () {
 //            $http.get("projects")
 //                    .then(function (response) {
-//                        $scope.showSousThemes = true;
-//                        $scope.showProjects = true;
 //                        $scope.projects = response.data;
 //                    });
 //        };
-        getProjects = function () {
+        getProjectsForEvents = function () {
             $http.get("projects")
                     .then(function (response) {
-                        $scope.showSousThemes = true;
-                        $scope.showProjects = true;
-                        $scope.projects = response.data;
+                        $scope.projectsEvents = response.data;
                     });
         };
-        setInterval(getProjects,1000);
+        setInterval(getProjectsForEvents, 5000);
 
         $scope.getProjectsFromSousTheme = function (id) {
             $http.get("projects/" + id)
                     .then(function (response) {
-                        $scope.showSousThemes = true;
-                        $scope.showProjects = true;
                         $scope.projects = response.data;
+                        $scope.projectsEvents = response.data;
                     });
         };
 
@@ -74,7 +63,6 @@ monAppli.controller('projectCtrl', [
         $scope.getTasks = function () {
             $http.get("tasks")
                     .then(function (response) {
-                        $scope.showTasks = true;
                         $scope.tasks = response.data;
                     });
         };
@@ -82,7 +70,6 @@ monAppli.controller('projectCtrl', [
         $scope.getTasksFromProject = function (id) {
             $http.get("tasks/" + id)
                     .then(function (response) {
-                        $scope.showTasks = true;
                         $scope.tasks = response.data;
                     });
         };
@@ -93,8 +80,6 @@ monAppli.controller('projectCtrl', [
             $http.get("posts")
 
                     .then(function (response) {
-                        $scope.showTasks = true;
-                        $scope.showPosts = true;
                         $scope.posts = response.data;
                     }
                     );
@@ -104,8 +89,6 @@ monAppli.controller('projectCtrl', [
             $http.get("posts/" + id)
 
                     .then(function (response) {
-                        $scope.showTasks = true;
-                        $scope.showPosts = true;
                         $scope.posts = response.data;
                     }
                     );
